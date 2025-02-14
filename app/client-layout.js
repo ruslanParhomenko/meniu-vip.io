f6d329891a73deb22c3ec7a3772cb8474d8a96f6
ruslan-parhomenko <parhomenkogm@gmail.com>
2025-02-14 22:33:06 +0200

    fix button prev next

--- app/client-layout.js
+++ app/client-layout.js
@@ -5,22 +5,13 @@ import { usePathname } from "next/navigation";
 
 export default function ClientLayout({ children }) {
   const pathname = usePathname();
-  console.log(pathname);
   return (
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
-        initial={
-          pathname === "/bar-page-2"
-            ? { opacity: 0, x: "-100%" }
-            : { opacity: 0, x: "100%" }
-        }
+        initial={{ opacity: 0, x: "100%" }}
         animate={{ opacity: 1, x: "0%" }}
-        exit={
-          pathname === "/bar-page-2"
-            ? { opacity: 0, x: "100%" }
-            : { opacity: 0, x: "-100%" }
-        }
+        exit={{ opacity: 0, x: "-100%" }}
         transition={{ duration: 0.5, ease: "easeInOut" }}
       >
         {children}
--- app/component/button-navigation-page.js
+++ app/component/button-navigation-page.js
@@ -1,22 +1,49 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function ButtonNavigationPage({ leftPage, rightPage }) {
+  const router = useRouter();
   const pathname = usePathname();
+
+  const getTextPrev = () => {
+    switch (pathname) {
+      case "/bar":
+        return "← home";
+      case "/bar-page-2":
+        return "← prev";
+      case "/cusine":
+        return "← home";
+      default:
+        return null;
+    }
+  };
+  const getTextNext = () => {
+    switch (pathname) {
+      case "/bar":
+        return "next →";
+      case "/bar-page-2":
+        return "home →";
+      case "/cusine":
+        return null;
+      default:
+        return null;
+    }
+  };
   return (
     <div className="py-4">
       <button
-        onClick={() => router.push({ leftPage })}
+        onClick={() => router.push(leftPage)}
         className={`text-xs absolute  left-12 `}
       >
-        {pathname === "/bar-page-2" ? "← prev" : "← home"}
+        {getTextPrev()}
       </button>
       <button
-        onClick={() => router.push({ rightPage })}
+        onClick={() => router.push(rightPage)}
         className={`text-xs absolute  right-12`}
       >
-        next →
+        {getTextNext()}
       </button>
     </div>
   );
